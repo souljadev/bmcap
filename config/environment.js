f075573b696c5abd0f80a2020da70ea7bd1903dc
souljadev <brendan.moy@gmail.com>
2018-01-12 10:31:13 -0500

    Initial commit
    -changed apihost in env environment from template to local host
    -changed rootURL to bmcap git repo

--- config/environment.js
+++ config/environment.js
@@ -3,10 +3,11 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'bmcap',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/bmcap';
     ENV.locationType = 'hash';
+    ENV.apiHost = '<% replace with the URL to your deployed API %>';
   }
 
   return ENV;
